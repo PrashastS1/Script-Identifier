b0eb5287429209080910f42419e525c72c1c21e2
Jenish Thakkar <jenishthakkar2004@gmail.com>
2025-04-08 15:49:12 +0530

    Minor Bug Fixes

--- models/XGBoost.py
+++ models/XGBoost.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import xgboost as xgb
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -42,65 +44,49 @@ if __name__ == '__main__':
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    # # Load Training Data
-    # train_dataset = BHSceneDataset(
-    #     root_dir="data/recognition",
-    #     train_split=True,
-    #     transform=None,
-    #     linear_transform=True,
-    #     backbone='resnet50',
-    #     gap_dim=1
-    # )
+    # Load Training Data
+    train_dataset = BHSceneDataset(
+        root_dir="data/recognition",
+        train_split=True,
+        transformation=True,
+        backbone='vit',
+    )
 
-    # X_train, y_train = extract_features(train_dataset)
+    X_train, y_train = extract_features(train_dataset)
 
-    # # Convert to DMatrix for XGBoost
-    # dtrain = xgb.DMatrix(X_train, label=y_train)
+    # Convert to DMatrix for XGBoost
+    dtrain = xgb.DMatrix(X_train, label=y_train)
 
-    # # Configure XGBoost Parameters
-    # params = {
-    #     "objective": "multi:softmax",  # Multi-class classification
-    #     "num_class": 14,  # Number of classes
-    #     "max_depth": 10,  # Limit tree depth to prevent overfitting
-    #     "learning_rate": 0.1,  # Default learning rate
-    #     "n_estimators": 100,  # Number of trees
-    #     "tree_method": "hist",  # Use GPU acceleration
-    #     "random_state": 42,
-    #     "device" : device
-    # }
+    # Configure XGBoost Parameters
+    params = {
+        "objective": "multi:softmax",  # Multi-class classification
+        "num_class": 14,  # Number of classes
+        "max_depth": 10,  # Limit tree depth to prevent overfitting
+        "learning_rate": 0.1,  # Default learning rate
+        "n_estimators": 100,  # Number of trees
+        "tree_method": "hist",  # Use GPU acceleration
+        "random_state": 42,
+        "device" : device
+    }
 
-    # # Train XGBoost on GPU
-    # clf = xgb.train(params, dtrain, num_boost_round=100)
+    # Load Test Data
+    test_dataset = BHSceneDataset(
+        root_dir="data/recognition",
+        train_split=False,
+        transformation=True,
+        backbone='vit',
+    )
 
-    # # Load Test Data
-    # test_dataset = BHSceneDataset(
-    #     root_dir="data/recognition",
-    #     train_split=False,
-    #     transform=None,
-    #     linear_transform=True,
-    #     backbone='resnet50',
-    #     gap_dim=1
-    # )
+    X_test, y_test = extract_features(test_dataset)
 
-    # X_test, y_test = extract_features(test_dataset)
-
-    # # Convert Test Data to DMatrix
-    # dtest = xgb.DMatrix(X_test)
-
-    # # Predict
-    # y_pred = clf.predict(dtest)
-
-    # # Compute Accuracy
-    # acc = accuracy(y_test, y_pred)
-    # print(f"Test Accuracy: {acc:.4f}")
+    # Convert Test Data to DMatrix
+    dtest = xgb.DMatrix(X_test)
 
     param_grid = {
         'n_estimators': [50, 100, 150],
         'max_depth': [5, 10, 15],
         'learning_rate': [0.01, 0.1, 0.3],  # Add learning rate
-        'backbone': ['resnet50'],
-        'gap_dim_resnet': [1, 3, 5, 7],
-        # 'gap_dim_vgg': [1, 4, 7, 10, 13]
+        'backbone': ['vit'],
     }
 
     best_acc = 0
@@ -112,51 +98,44 @@ if __name__ == '__main__':
 
     # Manual Grid Search
     for backbone in param_grid['backbone']:
-        gap_dims = param_grid['gap_dim_resnet'] if backbone == 'resnet50' else param_grid['gap_dim_vgg']
+
+        # Load data
+        torch.cuda.empty_cache()
         
-        for gap_dim in gap_dims:
-            # Load data
-            torch.cuda.empty_cache()
-            
-            train_dataset = BHSceneDataset(root_dir="data/recognition", train_split=True,
-                                        linear_transform=True, backbone=backbone, gap_dim=gap_dim)
-            X_train, y_train = extract_features(train_dataset)
-            dtrain = xgb.DMatrix(X_train, label=y_train)
+        train_dataset = BHSceneDataset(root_dir="data/recognition", train_split=True, transformation=True, backbone=backbone)
+        X_train, y_train = extract_features(train_dataset)
+        dtrain = xgb.DMatrix(X_train, label=y_train)
 
-            test_dataset = BHSceneDataset(root_dir="data/recognition", train_split=False,
-                                        linear_transform=True, backbone=backbone, gap_dim=gap_dim)
-            X_test, y_test = extract_features(test_dataset)
-            dtest = xgb.DMatrix(X_test)
+        test_dataset = BHSceneDataset(root_dir="data/recognition", train_split=False, transformation=True, backbone=backbone)
+        X_test, y_test = extract_features(test_dataset)
+        dtest = xgb.DMatrix(X_test)
 
-            # Iterate over all parameter combinations
-            for n_estimators, max_depth, learning_rate in product(param_grid['n_estimators'], 
-                                                                param_grid['max_depth'], 
-                                                                param_grid['learning_rate']):
-                clf = xgb.XGBClassifier(
-                    objective="multi:softmax",  # or "multi:softprob" if needed
-                    num_class=14,
-                    tree_method="hist",  # Use GPU Hist tree method
-                    random_state=42,
-                    n_estimators=n_estimators,
-                    max_depth=max_depth,
-                    learning_rate=learning_rate
-                )
-                clf.fit(X_train, y_train)
+        # Iterate over all parameter combinations
+        for n_estimators, max_depth, learning_rate in product(param_grid['n_estimators'], param_grid['max_depth'], param_grid['learning_rate']):
+            clf = xgb.XGBClassifier(
+                objective="multi:softmax",  # or "multi:softprob" if needed
+                num_class=14,
+                tree_method="hist",  # Use GPU Hist tree method
+                random_state=42,
+                n_estimators=n_estimators,
+                max_depth=max_depth,
+                learning_rate=learning_rate
+            )
+            clf.fit(X_train, y_train)
 
-                y_pred = clf.predict(X_test)
-                acc = accuracy_score(y_test, y_pred)
+            y_pred = clf.predict(X_test)
+            acc = accuracy_score(y_test, y_pred)
 
-                logger.info(f"Backbone: {backbone}, GAP: {gap_dim}, n_estimators: {n_estimators}, "
-                    f"Max Depth: {max_depth}, Learning Rate: {learning_rate}, Acc: {acc:.4f}")
+            logger.info(f"Backbone: {backbone}, n_estimators: {n_estimators}, "
+                f"Max Depth: {max_depth}, Learning Rate: {learning_rate}, Acc: {acc:.4f}")
 
-                results.append({'backbone': backbone, 'gap_dim': gap_dim, 'n_estimators': n_estimators,
-                                'max_depth': max_depth, 'learning_rate': learning_rate, 'accuracy': acc})
+            results.append({'backbone': backbone, 'n_estimators': n_estimators,
+                            'max_depth': max_depth, 'learning_rate': learning_rate, 'accuracy': acc})
 
-                # Track the best model
-                if acc > best_acc:
-                    best_acc = acc
-                    best_params = {'backbone': backbone, 'gap_dim': gap_dim, 
-                                'n_estimators': n_estimators, 'max_depth': max_depth, 'learning_rate': learning_rate}
+            # Track the best model
+            if acc > best_acc:
+                best_acc = acc
+                best_params = {'backbone': backbone, 'n_estimators': n_estimators, 'max_depth': max_depth, 'learning_rate': learning_rate}
 
     logger.success(f"\nBest Accuracy: {best_acc:.4f}, Best Params: {best_params}")
 
@@ -168,27 +147,6 @@ if __name__ == '__main__':
     plt.xlabel('Number of Estimators')
     plt.ylabel('Accuracy')
     plt.savefig(os.path.join(plots_dir, "xg_accuracy_vs_n_estimators.png"))
-    plt.show()
-
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x='backbone', y='accuracy', hue='gap_dim', data=results_df)
-    plt.title('XG Accuracy by Backbone and GAP Dimension')
-    plt.savefig(os.path.join(plots_dir, "xg_accuracy_by_backbone_gapdim.png"))
-    plt.show()
-
-    plt.figure(figsize=(12, 6))
-    sns.catplot(x='max_depth', y='accuracy', col='backbone', hue='gap_dim', kind='bar', data=results_df, height=5,
-                aspect=0.8)
-    plt.suptitle('XG Accuracy by Max Depth, Backbone, and GAP Dimension', y=1.02)
-    plt.savefig(os.path.join(plots_dir, "xg_accuracy_by_maxdepth_backbone_gapdim.png"))
-    plt.show()
-
-
-    heatmap_data = results_df.pivot_table(index='gap_dim', columns=['backbone', 'max_depth'], values='accuracy')
-    plt.figure(figsize=(12, 8))
-    sns.heatmap(heatmap_data, annot=True, fmt=".4f", cmap="YlGnBu")
-    plt.title('XG Accuracy Heatmap')
-    plt.savefig(os.path.join(plots_dir, "xg_accuracy_heatmap.png"))
     plt.show()
 
     plt.figure(figsize=(8, 6))
