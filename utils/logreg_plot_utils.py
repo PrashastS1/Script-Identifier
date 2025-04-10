d2ff519028c4e64cf429a601935e7f41e4f6e303
AurindumBanerjee <b23cs1006@iitj.ac.in>
2025-04-10 16:43:45 +0530

    Updated plotting function to include backbone in file name.

--- utils/logreg_plot_utils.py
+++ utils/logreg_plot_utils.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 
-def plot_decision_boundary(X, y, model, lang, save_dir):
+def plot_decision_boundary(X, y, model, lang, save_dir, backbone="unknown"):
     pca = PCA(n_components=2)
     X_reduced = pca.fit_transform(X)
 
@@ -20,13 +20,15 @@ def plot_decision_boundary(X, y, model, lang, save_dir):
     plt.contourf(xx, yy, Z, alpha=0.3, cmap=plt.cm.Paired)
     scatter = plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y, cmap=plt.cm.Paired, edgecolors='k')
     plt.legend(handles=scatter.legend_elements()[0], labels=["Not " + lang, lang])
-    plt.title(f"LogReg HOG Decision Boundary ({lang})")
+    plt.title(f"LogReg ({backbone}) Decision Boundary ({lang})")
     plt.xlabel("PC 1")
     plt.ylabel("PC 2")
 
     os.makedirs(save_dir, exist_ok=True)
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    plot_path = os.path.join(save_dir, f"{lang}_decision_boundary_{timestamp}.png")
+    plot_filename = f"{lang}_{backbone}_decision_boundary_{timestamp}.png"
+    plot_path = os.path.join(save_dir, plot_filename)
+
     plt.savefig(plot_path)
     plt.close()
     print(f"[INFO] Plot saved at: {plot_path}")
